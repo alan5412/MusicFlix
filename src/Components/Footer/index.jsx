d816a8a8fafa04116f03b73dade306b9eb4ec4f6
Alan Camarena <alanmisael54@gmail.com>
2024-07-15 22:44:47 -0600

    FooterUpdate

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,11 +1,11 @@
 import { Flex, Text,Image } from "@chakra-ui/react"
-
+import Logo from "../../../public/musicLogo.jpg"
 const Footer = ()=>{
     return(
            
              <Flex direction='column' bg="#1a1a1a" justifyContent='center' padding={4} alignItems={'center'}>
                 <Image
-                    src="./public/logo.png"
+                    src={Logo}
                     width='100px'
                 />
                 <Text textAlign='center' color='teal.50'>Desarrolado por <a href="https://github.com/alan5412"><b>Alan Camarena</b></a> - 2024</Text>    
