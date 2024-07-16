b844db268b6fa68707749773e01db08eaaa1e345
Alan Camarena <alanmisael54@gmail.com>
2024-07-15 22:43:06 -0600

    update4

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,8 @@ import { Box, Stack, Text, Flex,Spacer, Image} from "@chakra-ui/react"
 import { Button } from "@chakra-ui/react"
 import { Link } from "react-router-dom"
 
+import Logo from "../../../public/musicLogo.jpg"
+
 
 
 const Header =()=>{
@@ -9,7 +11,7 @@ const Header =()=>{
         <Flex justifyContent='space-between' color='White' bg='#1a1a1a' p='3' paddingX='10%' >
             <Box color='white' bg='#1a1a1a' >
                 <Image
-                    src="../../public/logo.png"
+                    src={Logo}
                     width='100px'
                 />
             </Box>
